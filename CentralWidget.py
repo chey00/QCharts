d61a99a9ce71c854b7d084666f5d3e9a77044769
Christian Hey <c.hey@sbs-herzogenaurach.de>
2025-10-28 12:01:42 +0100

    Übung 1

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCharts import QChartView, QChart, QLineSeries, QValueAxis
+from PyQt6.QtCharts import QChartView, QChart, QLineSeries, QValueAxis, QScatterSeries
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QColor
 
@@ -13,23 +13,40 @@ class CentralWidget(QChartView):
 
         axis_y = QValueAxis()
         axis_y.setTitleText("y-Achse")
-        axis_y.setRange(0, 16)
+        axis_y.setRange(0, 8)
 
         line_series = QLineSeries()
-        line_series.setName('Parabel')
+        line_series.setName('Gerade 1')
         line_series.append(0, 0)
-        line_series.append(1, 1)
+        line_series.append(1, 2)
         line_series.append(2, 4)
-        line_series.append(3, 9)
-        line_series.append(4, 16)
+        line_series.append(3, 6)
+        line_series.append(4, 8)
+
+        line_series_2 = QLineSeries()
+        line_series_2.setName('Gerade 2')
+        line_series_2.append(0, 3.0)
+        line_series_2.append(1, 3.5)
+        line_series_2.append(2, 4.0)
+        line_series_2.append(3, 4.5)
+        line_series_2.append(4, 5.0)
+
+        scatter_series = QScatterSeries()
+        scatter_series.setName("Schnittpunkt")
+        scatter_series.append(2, 4)
 
         chart = QChart()
         chart.addSeries(line_series)
+        chart.addSeries(line_series_2)
+        chart.addSeries(scatter_series)
         chart.addAxis(axis_x, Qt.AlignmentFlag.AlignBottom)
         chart.addAxis(axis_y, Qt.AlignmentFlag.AlignLeft)
 
         line_series.attachAxis(axis_x)
         line_series.attachAxis(axis_y)
+        line_series_2.attachAxis(axis_x)
+        line_series_2.attachAxis(axis_y)
+        scatter_series.attachAxis(axis_x)
+        scatter_series.attachAxis(axis_y)
 
         self.setChart(chart)
-
